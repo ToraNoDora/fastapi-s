c7d82b5760806d9bcbae4058c902a2aa23fe3555
Stepan Andreev <andreev.stepan@e-queo.net>
2022-06-12 22:58:31 +0300

    added Dockerfile and some scripts

---
+++ app/run.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+PRE_START_PATH=${PRE_START_PATH:-/prestart.sh}
+echo "Checking for script in $PRE_START_PATH"
+if [ -f $PRE_START_PATH ] ; then
+    echo "Running script $PRE_START_PATH"
+    . "$PRE_START_PATH"
+else
+    echo "There is no script $PRE_START_PATH"
+fi
+
+export APP_MODULE=${APP_MODULE-src.main:app}
+export HOST=${HOST:-0.0.0.0}
+export PORT=${PORT:-8001}
+
+
+# run gunicorn
+exec gunicorn --bind $HOST:$PORT "$APP_MODULE" -k uvicorn.workers.UvicornWorker
+
